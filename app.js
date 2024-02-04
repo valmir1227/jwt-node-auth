b5d93c659bda28f826c194de9e58f1826b8515a4
Valmir Almeida <almeidavalmir76@gmail.com>
2024-02-04 12:16:01 -0300

    Cookies Primer

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
-const dotenv = require('dotenv')
-require('dotenv').config()
+const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
 
@@ -28,3 +28,25 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// Cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("Set-cookie", "new-user=true")
+
+  res.cookie("newUser", false);
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+
+  res.send("You got the cookies!");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
